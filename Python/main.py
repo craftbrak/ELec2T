57862fff708671d257b9b3644ebf239f5b88001e
Hotmailnico <63592532+Hotmailnico@users.noreply.github.com>
2021-03-09 18:38:25 +0100

    up python kivy

--- Python/main.py
+++ Python/main.py
@@ -1,5 +1,11 @@
 import serial, sys
 import serial.tools.list_ports
+import kivy
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
 
 
 ser = serial.Serial('COM4', baudrate=9600, timeout=1)
@@ -13,5 +19,33 @@ def fct():
             print("Nombre de personne à l'intérieur : " + msg_split[1] + " " + msg_split[2])
 
 
+class MyApp(App):
+    def build(self):
+        self.afficheur = BoxLayout(orientation='vertical')
+
+        self.afficheur.add_widget(Label(text="A combien de personnes voulez-vous limiter l'accès ?"))
+
+        self.nombre_client = TextInput(text="")
+        self.afficheur.add_widget(self.nombre_client)
+
+        self.boutton = Button(text="Définir nombre client")
+        self.boutton.bind(on_press=self.voir)
+        self.afficheur.add_widget(self.boutton)
+
+        self.label_limite = Label(text="La limite du magasin est de 5 personnes")
+        self.afficheur.add_widget(self.label_limite)
+
+        self.label_actuel = Label(text="Il y a 0 personnes dans le magasin !")
+        self.afficheur.add_widget(self.label_actuel)
+        return self.afficheur
+
+    def voir(self, text):
+        if(self.nombre_client.text == ""):
+            print("False")
+        else:
+            print(self.nombre_client.text)
+            self.label_limite.text = f"La limite du magasin est de {self.nombre_client.text} personnes !"
+
+
 if __name__ == '__main__':
-    fct()
+    MyApp().run()
